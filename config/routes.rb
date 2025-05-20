b3206df656ec3558451ed6253fba4af419aab66c
Hadrien Senegas <hadrien.senegas@gmail.com>
2025-05-20 15:11:29 +0200

    exo1

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  # resources :tasks
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
   # root "posts#index"
 end
